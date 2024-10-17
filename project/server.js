86c3dbafd7e0f9013575e6698e048f4d22ee17ba
HarryEddward <he0780070@gmail.com>
2024-10-17 16:10:25 +0200

    correction from server.js changed from commonjs to es modules

--- project/server.js
+++ project/server.js
@@ -1,12 +1,22 @@
-// server.js
-const express = require('express');
-const path = require('path');
+// Importa los módulos de ES
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Obtiene la ruta del directorio actual (ya que __dirname no está disponible en ES modules)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // Sirve archivos estáticos desde la carpeta de producción de Vite
 app.use(express.static(path.join(__dirname, 'dist')));
+
+// Ruta de health check
+app.get('/healthcheck', (req, res) => {
+  res.status(200).json({ status: 'OK', message: 'Server is running smoothly' });
+});
 
 // Redirige todas las peticiones a la aplicación de React
 app.get('*', (req, res) => {
